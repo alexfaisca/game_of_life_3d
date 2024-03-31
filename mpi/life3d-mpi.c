9253c11edda0e5ab171478e1ae06ede93842da62
alexfaisca <alex.c.2002@gmail.com>
2024-03-31 20:22:21 +0100

    fix

--- mpi/life3d-mpi.c
+++ mpi/life3d-mpi.c
@@ -1,7 +1,3 @@
-/* DISCLAIMER */
-/* Made exclusively by Alexandre Coelho, student number 100120 */
-/* END OF DISCLAIMER */
-
 #include <mpi.h>
 #include <omp.h>
 #include <stdint.h>
@@ -59,8 +55,7 @@ char ***gen_initial_grid(int64_t n, float density, int input_seed,
   printf("RANK %d\n", rank);
 #endif
   if (rank != 0)
-    MPI_Recv(&seed, 1, MPI_UNSIGNED, rank - 1, 0, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
+    MPI_Recv(&seed, 1, MPI_UNSIGNED, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
   for (x = 0; x < actual_size; x++) {
     if (!(grid[x] = malloc(n * sizeof(char *)))) {
@@ -103,49 +98,36 @@ void synchronize_extremities(int64_t n, int world_size, int rank) {
   last_row = grid + n / world_size + (rank < n % world_size);
   if (rank == 0) {
     MPI_Send(&(last_row[0][0][0]), t, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
-    MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, 1, 0, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
+    MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     if (world_size % 2) {
-      MPI_Send(&(grid[1][0][0]), t, MPI_BYTE, world_size - 1, 0,
-               MPI_COMM_WORLD);
-      MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
+      MPI_Send(&(grid[1][0][0]), t, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD);
+      MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     } else {
-      MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
-      MPI_Send(&(grid[1][0][0]), t, MPI_BYTE, world_size - 1, 0,
-               MPI_COMM_WORLD);
+      MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Send(&(grid[1][0][0]), t, MPI_BYTE, world_size - 1, 0, MPI_COMM_WORLD);
     }
   } else if (rank == world_size - 1) {
     if (world_size % 2) {
       MPI_Send(&(grid[1][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD);
-      MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
-      MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, 0, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
+      MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       MPI_Send(&(last_row[0][0][0]), t, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
     } else {
-      MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
+      MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       MPI_Send(&(grid[1][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD);
       MPI_Send(&(last_row[0][0][0]), t, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
-      MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, 0, 0, MPI_COMM_WORLD,
-               MPI_STATUS_IGNORE);
+      MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
   } else if (rank % 2) {
-    MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
+    MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Send(&(grid[1][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD);
-    MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
+    MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Send(&(last_row[0][0][0]), t, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD);
   } else {
     MPI_Send(&(last_row[0][0][0]), t, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD);
-    MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
+    MPI_Recv(&(last_row[1][0][0]), t, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Send(&(grid[1][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD);
-    MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
+    MPI_Recv(&(grid[0][0][0]), t, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
 }
 
@@ -161,10 +143,8 @@ void print_grid(int64_t gen, int64_t n) {
     printf("Layer %ld:\n", x);
     for (y = 0; y < n; y++) {
       for (z = 0; z < n; z++) {
-        if (grid[x][y][z] != 0)
-          printf("%d ", grid[x][y][z]);
-        else
-          printf("  ");
+        if (grid[x][y][z] != 0) printf("%d ", grid[x][y][z]);
+        else printf("  ");
       }
       printf("\n");
     }
@@ -195,32 +175,27 @@ void simulation_synchronized(int64_t gen, int64_t n, int world_size, int rank) {
   if (world_size > 1) {
 #ifdef DEBUG
     printf("Before stat sync: ");
-    for (x = 0; x < N_SPECIES; x++)
-      printf("%ld ", stats[x].c);
+    for (x = 0; x < N_SPECIES; x++) printf("%ld ", stats[x].c);
     printf("\n");
 #endif
     for (x = 0; x < N_SPECIES; total[x] = stats[x].c, x++)
       ;
-    MPI_Reduce(total, count, N_SPECIES, MPI_INT64_T, MPI_SUM, 0,
-               MPI_COMM_WORLD);
+    MPI_Reduce(total, count, N_SPECIES, MPI_INT64_T, MPI_SUM, 0, MPI_COMM_WORLD);
 #ifdef DEBUG
     printf("After stat sync: ");
 #endif
     for (x = 0; x < N_SPECIES; total[x] = count[x] = 0, x++)
       stats[x] = (Statistics){count[x], 0}
 #ifdef DEBUG
-      ,
-      printf("%ld ", stats[x].c);
+      , printf("%ld ", stats[x].c);
     printf("\n");
 #else
       ;
 #endif
   }
   for (g = 0;; g++) {
-    if (world_size > 1)
-      synchronize_extremities(n, world_size, rank);
-    else
-      singleton_extremeties(n);
+    if (world_size > 1) synchronize_extremities(n, world_size, rank);
+    else singleton_extremeties(n);
 #ifdef DEBUG
     print_grid(g, n);
 #endif
@@ -231,19 +206,14 @@ void simulation_synchronized(int64_t gen, int64_t n, int world_size, int rank) {
         for (z = 0; z < n; z++) {
           neighbours = get_neighbours(n, x, y, z, count);
           for (s = 1, i = 1, c = 0; s <= N_SPECIES; count[s++] = 0)
-            if (count[s] > c)
-              i = s, c = count[s];
+            if (count[s] > c) i = s, c = count[s];
 
           if (grid[x][y][z] == 0) {
-            if (neighbours > 6 && neighbours < 11)
-              total[(grid[xx + 3][y][z] = i) - 1]++;
-            else
-              grid[xx + 3][y][z] = 0;
-          } else if (neighbours < 5 || neighbours > 13) {
-            grid[xx + 3][y][z] = 0;
-          } else {
-            total[(grid[xx + 3][y][z] = grid[x][y][z]) - 1]++;
-          }
+            if (neighbours > 6 && neighbours < 11) 
+	      total[(grid[xx + 3][y][z] = i) - 1]++;
+            else grid[xx + 3][y][z] = 0;
+          } else if (neighbours < 5 || neighbours > 13) grid[xx + 3][y][z] = 0;
+          else total[(grid[xx + 3][y][z] = grid[x][y][z]) - 1]++;
         }
       }
       if (x > 1) {
@@ -259,27 +229,22 @@ void simulation_synchronized(int64_t gen, int64_t n, int world_size, int rank) {
       } else { // case x == 1
         if (x == xx) {
           swap_space = grid[x];
-          grid[x] = grid[xx + 3];
-          grid[xx + 3] = swap_space;
+	  grid[x] = grid[xx + 3];
+	  grid[xx + 3] = swap_space;
         } else {
-          swap_space = grid[xx + 2];
-          grid[xx + 2] = grid[xx + 3];
-          grid[xx + 3] = swap_space;
+          swap_space = grid[xx + 2], grid[xx + 2] = grid[xx + 3]; 
+	  grid[xx + 3] = swap_space;
         }
       }
     }
 
     if (world_size > 1) {
-      MPI_Reduce(total, count, N_SPECIES, MPI_INT64_T, MPI_SUM, 0,
-                 MPI_COMM_WORLD);
+      MPI_Reduce(total, count, N_SPECIES, MPI_INT64_T, MPI_SUM, 0, MPI_COMM_WORLD);
       for (x = 0; x < N_SPECIES; total[x] = count[x] = 0, x++)
         if (count[x] > stats[x].c && rank == 0)
           stats[x] = (Statistics){count[x], g + 1};
-    } else {
-      for (x = 0; x < N_SPECIES; total[x] = 0, x++)
-        if (total[x] > stats[x].c)
-          stats[x] = (Statistics){total[x], g + 1};
-    }
+    } else for (x = 0; x < N_SPECIES; total[x] = 0, x++)
+        if (total[x] > stats[x].c) stats[x] = (Statistics){total[x], g + 1};
   }
   return;
 }
@@ -302,14 +267,15 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
   grid = gen_initial_grid(n, density, seed, world_size, world_rank);
-  if (world_size == 1 || world_rank == 0)
-    exec_time = -omp_get_wtime();
+  if (world_size == 1 || world_rank == 0) exec_time = -omp_get_wtime();
+
   simulation_synchronized(gen, n, world_size, world_rank);
+
   if (world_size == 1 || world_rank == 0)
     exec_time += omp_get_wtime(), print_result(),
         fprintf(stderr, "%.1fs\n", exec_time);
   destroy_grid(n, world_size, world_rank);
-  MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Finalize();
+
+  MPI_Barrier(MPI_COMM_WORLD), MPI_Finalize();
   return 0;
 }
